9e041ad8af2141c8638850c8d6cee1dc3a5340d9
Mohammad Imran <mohammad.imran@diverselynx.com>
2022-08-23 17:15:20 +0530

    Formik Validiation
    Signed-off-by: Mohammad Imran <mohammad.imran@diverselynx.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {useFormik} from 'formik'
+import * as Yup from 'yup';
+
 
 function App() {
+
+  const [login, setLogin] = useState(false)
+
+  const formik = useFormik({
+    validationSchema: Yup.object().shape({
+      firstname:Yup.string().label('First Name are reuired').required(),
+      lastname:Yup.string().label('Last Name are reuired').required(),
+      email: Yup.string().email('Required email').required()
+
+    }),
+    initialValues:{
+      firstname: '',
+      lastname: '',
+      email:''
+    },
+    onSubmit : (values) =>{
+      setLogin(true)
+    }
+
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App app-container">
+      <form onSubmit={formik.handleSubmit}>
+    <div className='input-fld'>
+      <label>First Name</label>
+      <input type="text" name="firstname" id="firstname" value={formik.values.firstname} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+      {formik.errors.firstname ? <label className='lbl-error'>{formik.errors.firstname}</label> : null }
+    </div>
+    <div className='input-fld'>
+      <label>Last Name</label>
+      <input type="text" name="lastname" id="lastname" value={formik.values.lastname} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+      {formik.errors.lastname ? <label className='lbl-error'>{formik.errors.lastname}</label> : null }
+    </div>
+
+    <div className='input-fld'>
+      <label>Email</label>
+      <input type="text" name="email" id="email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+      {formik.errors.email ? <label className='lbl-error'>{formik.errors.email}</label> : null }
+    </div>
+    <input type="submit" value="Submit" />
+
+      </form>
+
+      {
+
+        login ? 
+        <p className='welcom'>Congats Your Welcom again</p>
+        : null
+      }
+     
     </div>
   );
 }
